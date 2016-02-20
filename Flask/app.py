3e0bc306993c70685e0e0bb33e11bf3235443a43
Fondson <fondsont@gmail.com>
2016-02-20 17:22:52 -0500

    sync

--- Flask/app.py
+++ Flask/app.py
@@ -13,6 +13,12 @@ readFile = open("lib3.txt","r")
 line = readFile.readline()
 wordArray = line.split()
 print(wordArray)
+readFile = open("lib3.txt",'r')
+line = readFile.readline()
+stringArray = line[line.find(" "):].split()
+userKeyWordArray = (stringArray[0][1:-1]).split(",")
+print(stringArray)
+
 # Define a route for the default URL, which loads the form
 @app.route('/')
 def form():
@@ -23,16 +29,8 @@ def form():
 # accepting: POST requests in this case
 @app.route('/hello/', methods=['POST'])
 def hello():
-<<<<<<< HEAD
-=======
 
-	readFile = open("/Users/FrankMAC/Desktop/Flask/lib3.txt")
-	line = readFile.readline()
-	stringArray = line[line.find(" "):].split()
-	userKeyWordArray = (stringArray[0][1:-1]).split(",")
-	print(stringArray)
-
->>>>>>> origin/master
+	
 	name=request.form['yourname']
 	email=request.form['youremail']
 	return render_template('form_action.html', name=name, email=email)
